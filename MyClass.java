548b86684c047bf111fe79b8cc0e7ae74f0474c2
Arshita640 <70799814+Arshita640@users.noreply.github.com>
2020-09-11 01:33:16 +0530

    Update MyClass.java

--- MyClass.java
+++ MyClass.java
@@ -1,7 +1,7 @@
 public class MyClass {
   public static void main(String[] args) {
-    int x = 5;
-    int y = 6;
+    int x = 3;
+    int y = 7;
     int sum = x + y;
     System.out.println(sum); // Print the sum of x + y
   }
