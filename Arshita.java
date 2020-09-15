df5fa359bd7906654751aad80516c2da4c7b9ded
Arshita <arshita2iti@gmail.com>
2020-09-16 00:12:13 +0530

    Arshita

---
+++ Arshita.java
@@ -0,0 +1,8 @@
+public class Arshita {
+  public static void main(String[] args) {
+    int x = 7;
+    int y = 8;
+    int sum = x + y;
+    System.out.println(sum); // Print the sum of x + y
+  }
+}
